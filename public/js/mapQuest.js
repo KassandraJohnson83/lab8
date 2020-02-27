9533385bd3940a56d03728765189bf367ba7ead7
KassandraJohnson83 <59705381+KassandraJohnson83@users.noreply.github.com>
2020-02-27 12:54:37 -0800

    Update mapQuest.js

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	.mapquest.key = ylvpQAthDuXX9mYGVA3NQvG17dYhU6Gz;
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [37.7749, -122.4194],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+  zoomControl: false
+});
+L.marker([32.88, -117.236]).addTo(map);
 }
